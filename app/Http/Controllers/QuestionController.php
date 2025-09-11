85d7202ad1a37c56fb2306dd59ba0eb8889cf48a
dispropele <dispropele@gmail.com>
2025-09-11 22:46:50 +0300

    реализовано добавление вопроса

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\QuestionStatus;
+use App\Http\Requests\Question\CreateQuestionRequest;
 use App\Models\Question;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -23,9 +26,49 @@ class QuestionController extends Controller
         return view('question.show', $question);
     }
 
-    public function create(Request $request)
+    public function create(CreateQuestionRequest $request)
     {
+        $validated = $request->validated();
 
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $extension = $file->getClientOriginalExtension();
+            $fileNameToStore = $filename . '_' . time() . '.' . $extension;
+
+            $path = $file->storeAs("public/questions", $fileNameToStore);
+        }
+
+        // Создаем вопрос
+        $question = auth()->user()->questions()->create([
+            'title' => $validated['title'],
+            'body' => $validated['body'],
+            'image_path' => $path ?? null,
+            'status' => QuestionStatus::Open
+        ]);
+
+        //Обработка тегов
+        $tagIds = [];
+
+        //Проверяем наличие тегов
+        if ($request->has('tags') && !empty($request->tags)) {
+            // Разбиваем строку на массив
+            $tagNames = array_map('trim', explode(',', $request->tags));
+
+            foreach ($tagNames as $tagName) {
+                //Ищем тег в базе или создаем новый
+                $tag = Tag::firstOrCreate(['name' => $tagName]);
+                //Заполняем массив тегов для синхронизации
+                $tagIds[] = $tag->id;
+            }
+        }
+
+        //Синхронизуем теги с вопросом
+        if (!empty($tagIds)) {
+            $question->tags()->sync($tagIds);
+        }
+
+        return redirect()->route('question.show', $question);
     }
 
 }
--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -60,7 +60,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                         </svg>
                         <p class="mt-2 text-brutal-primary">Перетащите или нажмите, чтобы загрузить</p>
-                        <p class="text-xs text-brutal-secondary">PNG, JPG, GIF до 10MB</p>
+                        <p class="text-xs text-brutal-secondary">PNG, JPG, GIF до 5MB</p>
                     </div>
 
                     <img id="image-preview" src=""
@@ -73,14 +73,118 @@
                        id="image-upload" class="hidden">
             </div>
 
+            <div>
+                <label class="font-semibold">
+                    Теги (макс. 5)
+                </label>
+                {{--                Пойдет в контроллер--}}
+                <input type="hidden" name="tags" id="tags-input-hidden">
+
+                {{--                Контейнер для тегов--}}
+                <div id="tags-container" class="mt-2 flex items-center flex-wrap gap-2 p-2 border-2">
+                    {{--                    js будет вставлять сюда добавленые теги--}}
+                    <input type="text" id="tag-input-text"
+                           class="flex-grow bg-transparent focus:outline-none min-w-[120px]"
+                           placeholder="Добавьте тег...">
+                </div>
+                <p id="tag-error" class="text-brutal-red text-sm mt-1 hidden"></p>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <button type="submit"
+                        class="bg-red-600 text-white font-semibold text-xl
+                        py-2 px-8 border-2 hover:bg-red-700 transition-colors
+                        focus:outline-none focus:ring-2 focus:ring-offset-zinc-800
+                        focus:ring-red-500 cursor-pointer">
+                    Создать
+                </button>
+            </div>
+
         </form>
     </div>
 
     <script>
+        //Константы для изображения
         const imageUploadInput = document.getElementById('image-upload')
         const imagePreview = document.getElementById('image-preview')
         const placeholder = document.getElementById('placeholder')
+        //Константы для тегов
+        const tagsContainer = document.getElementById('tags-container')
+        const textInput = document.getElementById('tag-input-text')
+        const hiddenInput = document.getElementById('tags-input-hidden')
+        const tagError = document.getElementById('tag-error')
+        const MAX_TAGS = 5
 
+        //Выбраные теги пользователем
+        let tags = []
+
+        //Функция для отрисовки тегов в контейнере
+        const renderTags = () => {
+            //Очищаем старые теги
+            tagsContainer.querySelectorAll('.tag').forEach(tagEl => tagEl.remove())
+
+            //Рисуем теги заново
+            tags.slice().reverse().forEach((tag, index) => {
+                const tagElement = createTagElement(tag)
+                tagsContainer.prepend(tagElement)
+            })
+
+            hiddenInput.value = tags.join(',')
+            tagError.classList.add('hidden')
+        }
+
+        //Функция для создания html элемента тега
+        const createTagElement = (tag) => {
+            const div = document.createElement('div')
+            div.className = 'tag bg-gray-200 text-black ' +
+                'text-sm font-semibold px-3 py-1 ' +
+                'rounded-full flex items-center';
+
+            const span = document.createElement('span')
+            span.textContent = tag
+
+            const closeBtn = document.createElement('button')
+            closeBtn.className = 'ml-2 text-black hover:text-red-500 font-bold'
+            closeBtn.innerHTML = '&times;' // Символ x
+            closeBtn.addEventListener('click', () => {
+                tags = tags.filter(t => t !== tag)
+                renderTags()
+            })
+
+            div.appendChild(span)
+            div.appendChild(closeBtn)
+            return div
+        }
+
+        //Слушатель тегов
+        textInput.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter' || event.key === ',') {
+                event.preventDefault() //Предотвращаем отправку формы или ввод запятой
+
+                const newTag = textInput.value.trim()
+
+                if (newTag.length > 0) {
+                    if (tags.length >= MAX_TAGS) {
+                        tagError.textContent = `Можно добавить не более ${MAX_TAGS} тегов.`
+                        tagError.classList.remove('hidden')
+                        return;
+                    }
+                    if (tags.includes(newTag)) {
+                        tagError.textContent = 'Такой тег уже добавлен.'
+                        tagError.classList.remove('hidden')
+                        return;
+                    }
+
+                    tags.push(newTag)
+                    textInput.value = ''
+                    tagError.classList.add('hidden')
+                    renderTags()
+                }
+
+            }
+        })
+
+        //Обработка выбора фотки
         imageUploadInput.addEventListener('change', (event) => {
             const file = event.target.files[0]
 
